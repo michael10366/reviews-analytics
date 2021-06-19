ae26a8b29620e6f0ef09191a4737e411e7d62d91
michael wu <michael10366@outlook.com>
2021-06-19 16:04:34 +0800

    add filtering

--- read.py
+++ read.py
@@ -29,3 +29,9 @@ sum_len = 0
 for d in data:
 	sum_len += len(d)
 print('留言平均長度為', sum_len / len(data))
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '筆資料留言小於100')
