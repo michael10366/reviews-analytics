631ee6d3d9e2bd246bce9ca13a082c36bc8cdb69
michael wu <michael10366@outlook.com>
2021-06-30 23:52:22 +0800

    add word count

--- read.py
+++ read.py
@@ -1,37 +1,45 @@
-#reviews-analytics
+#read
 
-data = []
-count = 0
-#count1 = 0
-#strlen = 0
-#c = 0
+def read_file(filename):
+	data = []
+	count = 0
+	with open(filename, 'r', encoding = 'UTF-8-sig') as f:
+		for line in f:
+			data.append(line)
+			count += 1
+			# if count % 100000 == 0:
+			# 	print(len(data))
+		print('檔案讀取完了,總共有', len(data), '筆資料')
+		return data
 
-with open('reviews.txt', 'r') as f:
-	for line in f:
-		data.append(line)
-		count += 1
-		if count % 1000 ==0:
-			print(len(data))
 
-print('檔案讀取玩了,總共有', len(data), '筆資料')
 
-			#data = []  (selfmade)
-			#with open('reviews.txt', 'r') as f:
-			#	for line in f:
-			#		count1 += 1
-			#		data.append(line)
-			#		c = len(data[count1 - 1])
-			#		strlen = strlen + c
-			#l = len(data)
-								
-			#print('每筆留言平均長度是', strlen / l, '字')
-sum_len = 0
+data = read_file('reviews.txt')
+
+wc = {}
 for d in data:
-	sum_len += len(d)
-print('留言平均長度為', sum_len / len(data))
+	words = d.split()
+	for word in words:
+		if word in wc:
+			wc[word] += 1
+		else:
+			wc[word] = 1
 
-new = []
-for d in data:
-	if len(d) < 100:
-		new.append(d)
-print('一共有', len(new), '筆資料留言小於100')
+
+for word in wc:
+	if wc[word] > 1000000:
+		print(word, wc[word])
+
+
+while True:
+	userinput = input("請輸入想要查找的字，按q離開: ")
+	if userinput == 'q':
+		break
+	elif userinput in wc:
+		print(userinput, '共出現過', wc[userinput], '次' )
+	else:
+		print('你查找的字不再字典裡')
+
+print('感謝使用查詢功能')
+
+
